99e8a26668c95955e594f4108fe7aa5096905367
danthemanr <danthemanr123@gmail.com>
2025-03-07 12:10:07 -0700

    lab6 src2

---
+++ Lab06/src2/About.js
@@ -0,0 +1,12 @@
+
+function About({title, description}) {
+  return (
+    <div>
+      <p>About Us</p>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default About;
