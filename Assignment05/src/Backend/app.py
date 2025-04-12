2b91816e951960d789fa8a8fee3bd3038bff5980
danthemanr <danthemanr123@gmail.com>
2025-04-11 19:55:58 -0600

    assignment 5 one small bug

--- Assignment05/src/Backend/app.py
+++ Assignment05/src/Backend/app.py
@@ -126,7 +126,7 @@ def drop(student_id):
 
 @app.route('/courses', methods=['GET']) #TODO: Homepage.js, MainSection.js, and CoursePage.js need to be debugged
 def courses():
-    with open('testimonials.json', 'r') as f:
+    with open('courses.json', 'r') as f:
         return ''.join([l.strip() for l in f])
 
 @app.route('/student_courses/<student_id>', methods=['GET'])
